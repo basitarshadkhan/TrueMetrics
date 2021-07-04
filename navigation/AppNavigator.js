5c4c968f1ef606a0a5dba424722ce332d1b751f3
Basit Arshad <basit.arshad@graana.com>
2021-07-04 13:43:58 +0500

    Navigation setup

---
+++ navigation/AppNavigator.js
@@ -0,0 +1,22 @@
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from "../screens/Home";
+
+const Stack = createStackNavigator();
+
+const MyStack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ header: () => null }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default MyStack;
